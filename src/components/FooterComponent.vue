ad9075932b125193d1f4c9c5d0409dfa98906241
pablocheli2001 <pablocheli2001@gmail.com>
2024-05-30 20:41:30 +0000

    B91583

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="mt-12 bg-gray-100 py-4 text-center">
+      <h5 class="font-semibold text-lg">Acme - 5 Member Team</h5>
+    </footer>
+  </template>
+  
+  <script setup>
+  </script>
+  
---
+++ src/components/TeamTableComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="place-self-center flex flex-col gap-y-3">
+      <table class="table-fixed border-spacing-2 border-separate">
+        <thead>
+          <th class="text-left px-6 py-2">Name</th>
+          <th class="text-left px-6 py-2">Email</th>
+          <th class="text-left px-6 py-2">Status</th>
+        </thead>
+        <tbody>
+          <MemberRowComponent v-for="member in members" :key="member.email" :member="member" />
+        </tbody>
+      </table>
+      <p class="text-right text-gray-600 italic">There are no remaining team spots. Upgrade to add more.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import MemberRowComponent from './MemberRowComponent.vue';
+  
+  const props = defineProps({
+    members: {
+      type: Array,
+      required: true
+    }
+  });
+  </script>
+  
